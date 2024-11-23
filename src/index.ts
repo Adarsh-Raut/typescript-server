53cbc159e2b6d323f82626b6dfabbccee4fbfbef
Adarsh-Raut <adarshraut0987@gmail.com>
2024-11-23 23:19:50 +0530

    package.json setup done

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
